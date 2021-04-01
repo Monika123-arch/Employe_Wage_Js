6c3b62e8b7c4865173af419b96ad6372fb330ebb
Monika123-arch <77197618+Monika123-arch@users.noreply.github.com>
2021-04-01 12:35:04 +0530

    Update EmployeeWage.js

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,16 +1,3 @@
-//UC1
-
-const IS_ABSENT = 0;
-
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck == IS_ABSENT){
-    console.log("Employee is absent");
-    return;
-}
-else {
-    console.log("Employee is present");
-}
-
 //UC2
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
